7dbd2b7b8ae8645a8bc869f76a53c09df2fee718
Zehra <78693714+zhrzkn@users.noreply.github.com>
2022-07-11 15:01:05 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+int main(void)
+{
+  RCC_DeInit(); // HSI ON | HSE OFF PLL OFF | SystemClock 16 MHz
+
+  while (1)
+  {
+
+  }
+}
+
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
