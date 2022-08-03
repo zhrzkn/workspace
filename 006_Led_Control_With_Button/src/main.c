91fafdd858d349628bb3069ab7394d0ffea0f6fc
Zehra <78693714+zhrzkn@users.noreply.github.com>
2022-08-03 12:30:20 +0300

    initial commit

---
+++ 006_Led_Control_With_Button/src/main.c
@@ -0,0 +1,59 @@
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+/* Private macro */
+/* Private variables */
+/* Private function prototypes */
+/* Private functions */
+
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
+int main(void)
+{
+  int i = 0;
+
+
+  /* TODO - Add your application code here */
+
+  /* Initialize LEDs */
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);
+  STM_EVAL_LEDInit(LED5);
+  STM_EVAL_LEDInit(LED6);
+
+  /* Turn on LEDs */
+  STM_EVAL_LEDOn(LED3);
+  STM_EVAL_LEDOn(LED4);
+  STM_EVAL_LEDOn(LED5);
+  STM_EVAL_LEDOn(LED6);
+
+  /* Infinite loop */
+  while (1)
+  {
+	i++;
+  }
+}
+
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
