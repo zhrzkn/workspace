baa86aebbfa95b07290831a14b1ce10eba5733f5
Zehra <78693714+zhrzkn@users.noreply.github.com>
2022-08-21 12:35:18 +0300

    initial commit

---
+++ 011_HAL_Led_Control_With_Button_External/Debug/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+MiddleWares/Third_Party/FreeRTOS/org/Source/portable/MemMang/heap_4.o: \
+ ../MiddleWares/Third_Party/FreeRTOS/org/Source/portable/MemMang/heap_4.c \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/FreeRTOSConfig/FreeRTOSConfig.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/FreeRTOSConfig/FreeRTOSConfig.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h:
---
+++ 011_HAL_Led_Control_With_Button_External/Debug/MiddleWares/Third_Party/FreeRTOS/org/Source/stream_buffer.d
@@ -0,0 +1,22 @@
+MiddleWares/Third_Party/FreeRTOS/org/Source/stream_buffer.o: \
+ ../MiddleWares/Third_Party/FreeRTOS/org/Source/stream_buffer.c \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/FreeRTOSConfig/FreeRTOSConfig.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h \
+ C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/stream_buffer.h
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/FreeRTOSConfig/FreeRTOSConfig.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/portable.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/task.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/list.h:
+C:/Users/MONSTER/Desktop/workspace/011_HAL_Led_Control_With_Button_External/MiddleWares/Third_Party/FreeRTOS/org/Source/include/stream_buffer.h:
